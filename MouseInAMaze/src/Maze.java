107316f73939500546cb78aa75c4e3b53d5f9a1b
miiegale <admin@10.99.24.2>
2013-08-28 16:28:57 +0700

    read jaa

--- MouseInAMaze/src/Maze.java
+++ MouseInAMaze/src/Maze.java
@@ -1,6 +1,25 @@
+import java.io.*;
 
 public class Maze {
 	public static void main(String arg[]){
-		System.out.print("Hi! Kookai");
+		
+		try
+		{
+            FileInputStream read = new FileInputStream("input.txt");
+            DataInputStream input = new DataInputStream(read);
+            BufferedReader br = new BufferedReader( new InputStreamReader(input));
+            String line="";
+            	while((line = br.readLine()) != null)
+            		{
+            			System.out.println(line);
+            		}
+            input.close();
+        }
+		
+		catch(Exception e)
+		{
+          	System.out.println("Error while reading the file:" + e.getMessage());
+        }
+
 	}
 }
