59ab643eeb6e89c0c5678ccadc917ccc2f170fdf
KOOOKAI <kookai.pattaraporn@gmail.com>
2013-08-28 21:11:48 +0700

    Update Maze.java

--- MouseInAMaze/src/Maze.java
+++ MouseInAMaze/src/Maze.java
@@ -1,25 +1,59 @@
 import java.io.*;
 
 public class Maze {
-	public static void main(String arg[]){
-		
+	public String mousePos;
+	public String cheesePos;
+	public String[] trapPos;
+	public int[][] rewards;
+	
+	public static void main(String arg[]){	
+		String mousePos = new String();
+		String cheesePos = new String();
 		try
 		{
-            FileInputStream read = new FileInputStream("input.txt");
+            FileInputStream read = new FileInputStream("C:\\Users\\HP\\Desktop\\input.txt");
             DataInputStream input = new DataInputStream(read);
-            BufferedReader br = new BufferedReader( new InputStreamReader(input));
-            String line="";
-            	while((line = br.readLine()) != null)
+            BufferedReader br = new BufferedReader(new InputStreamReader(input));
+            String board = "";
+            int line = 1;
+            	while((board = br.readLine()) != null)
             		{
-            			System.out.println(line);
+            			if(line==1){
+            				mousePos = board;
+            				System.out.println(mousePos);
+            			}
+            			if(line==2){
+            				cheesePos = board;
+            				System.out.println(cheesePos);
+            			}
+            			
+            			if(line==3){
+            					String[] trapPos = board.split(" ");
+            					System.out.println(trapPos);
+            				}
+            			line++;
             		}
+            			
+            			/*if(line==4){
+        					String[]  = board.split("\\s+");
+        					System.out.println(trapPos[]);
+        				}
+            				
+            			}
+            			
+            			line++;
+           			System.out.println(board);
+          			
+         			String hole[] = board.split(" ");
+       */     	
+            	
             input.close();
-        }
+		
+		}
 		
 		catch(Exception e)
 		{
-          	System.out.println("Error while reading the file:" + e.getMessage());
+          	System.out.println("Cannot read a text file:" + e.getMessage());
         }
-
 	}
 }
