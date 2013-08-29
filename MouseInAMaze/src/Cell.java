033eea264678913449983d8a45e4ae7cc9911969
CappucinosaN <lt.cappucinosan@gmail.com>
2013-08-29 20:43:57 +0700

    Update Cell.java
    Complete

--- MouseInAMaze/src/Cell.java
+++ MouseInAMaze/src/Cell.java
@@ -1,14 +1,48 @@
-
 public class Cell {
-	String position;
-	int reward;
+	public static final int SIZE = 9;
 	
-	public Cell(int row, char col){
+	public int row;
+	public int col;
+	public String position;
+	public int reward;
+	public boolean isTrap;
+	public boolean visited = false;
+	
+	public Cell(){}
+	
+	public Cell(String p){
+		position = p;
+		String tmp = position.substring(0,1);
+		row = Integer.parseInt(tmp)-1;
+		char tmpc = position.charAt(1);
+		char c = 'A';
+		
+		for(int i=0;i<SIZE;i++){
+			if(c==tmpc){
+				col = i;
+				break;
+			}
+			c++;
+		}
+	}
+	
+	public Cell(int r, int c, int w){
+		row = r+1;
+		col = c;
+		reward = w;
 		String strrow = Integer.toString(row);
-		position = strrow+col;
+		char cc = 'A';
+		for(int i=0;i<SIZE;i++){
+			if(col==i){
+				position = strrow.concat(Character.toString(cc));
+				break;
+			}
+			cc++;
+		}
 	}
 	
-	void displayPos(){
-		System.out.print(position);
+	public void setTrap(){
+		isTrap = true;
 	}
+	
 }
